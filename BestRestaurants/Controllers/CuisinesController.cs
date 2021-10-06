11ef4136832a0d03945b0d31583b834e01e7f082
Ben Wilson <benjaminw1030@gmail.com>
2021-10-06 10:37:11 -0700

    set up models Co-authored by: Ben Dunham <bendunhampdx@gmail.com>

---
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  public class CuisinesController : Controllers
+  {
+
+  }
+}
